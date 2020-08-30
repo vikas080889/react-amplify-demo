6d1ae27ab323e68dbe0e774ff63aa496ca05b396
admin <bestsharesin@gmail.com>
2020-08-30 13:25:16 +0530

    adding storage and api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_Usx18TixI",
     "aws_user_pools_web_client_id": "54460r47mieqhnf37vggnrdl5m",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://meaumarl4vaydb3foay3qya65a.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-kfxvgtvmk5aylhhy56ttnyev3q",
+    "aws_user_files_s3_bucket": "amplifyapp29480c87e2454765ae1e5e50e7fcc007112224-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
